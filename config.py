4c6195d950dd2dae4cc315ad64ae95ebb8cb889f
nishaddeokar <116658820+nishaddeokar@users.noreply.github.com>
2024-08-22 09:00:40 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'OBza57LssDbuK6JgKCjUXxsmlcRVMwLM'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
     SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
     SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
