171d5e8ed66c5f0a8cede948a9e8c02680080846
nishaddeokar <116658820+nishaddeokar@users.noreply.github.com>
2024-08-22 09:00:11 +0100

    Update terraAuth.py

--- terraAuth.py
+++ terraAuth.py
@@ -3,9 +3,6 @@ from terra.base_client import Terra
 import webbrowser
 
 def set(ref_id):
-    API_KEY = "020f5b152d76c975fd256ea2a81b520938b274dc73413b45fa231baee64a1018"
-    DEV_ID = "impeatrial-dev-R4tRG6iTLV"
-    SECRET = "bef95ca4ced3d6b9fb114baa3aa85d0be022818668fa3ea7"
 
     terra = Terra(API_KEY, DEV_ID, SECRET)
 
